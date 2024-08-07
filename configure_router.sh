009d7558a4ed535c09c15fab2d8d9dff2387a131
AbbyyAL <2204471C@student.tp.edu.sg>
2024-08-07 16:16:10 +0800

    Update configure_router.sh

--- configure_router.sh
+++ configure_router.sh
@@ -46,7 +46,7 @@ sudo docker exec -i -u root clab-firstlab-csr-r1 bash -c 'cat <<EOF > /configure
     - name: Set hostname
       ios_config:
         lines:
-          - hostname abigail
+          - hostname yanyanhafeezah
     - name: Ensure SSH is enabled
       ios_config:
         lines:
